9f7694098ea9b780d2838ce0a6be5703f7a0a3bf
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-04-09 15:19:55 -0300

    Initial commit with basic project scaffold and prisma

---
+++ src/app.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    await this.prisma.notification.create({
+      data: {
+        content: 'Você tem uma nova solicitação de amizade',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
+  }
+}
