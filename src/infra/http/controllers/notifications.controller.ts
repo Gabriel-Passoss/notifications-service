fa0749de9271d74836d53ec903c00dc028694e33
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-04-15 01:54:51 -0300

    Create routes to connect application to services

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,11 +1,25 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
-import { SendNotification } from '@app/services/send-notification-service';
 import { NotificationViewModel } from '../view-models/notification-view-models';
 
-@Controller('notifications')
+import { SendNotification } from '@app/services/send-notification-service';
+import { CancelNotification } from '@app/services/cancel-notification-service';
+import { ReadNotification } from '@app/services/read-notification-service';
+import { UnreadNotification } from '@app/services/unread-notification-service';
+import { CountRecipientNotifications } from '@app/services/count-recipient-notifications-service';
+import { GetRecipientNotifications } from '@app/services/get-recipient-notifications-service';
+
+@Controller('notification')
 export class NotificationsController {
-  constructor(private sendNotification: SendNotification) {}
+  constructor(
+    private sendNotification: SendNotification,
+    private cancelNotification: CancelNotification,
+    private readNotification: ReadNotification,
+    private unreadNotification: UnreadNotification,
+    private countRecipientNotifications: CountRecipientNotifications,
+    private getRecipientNotifications: GetRecipientNotifications,
+  ) {}
+
   @Post()
   async create(@Body() body: CreateNotificationBody) {
     const { recipientId, content, category } = body;
@@ -17,5 +31,48 @@ export class NotificationsController {
     });
 
     return { notification: NotificationViewModel.toHTTP(notification) };
+  }
+
+  @Get('/count-from/:recipientId')
+  async countFromRecipient(@Param('recipientId') recipientId: string) {
+    const { count } = await this.countRecipientNotifications.execute({
+      recipientId,
+    });
+
+    return {
+      count,
+    };
+  }
+
+  @Get('/from/:recipientId')
+  async getFromRecipient(@Param('recipientId') recipientId: string) {
+    const { notifications } = await this.getRecipientNotifications.execute({
+      recipientId,
+    });
+
+    return {
+      notifications: notifications.map(NotificationViewModel.toHTTP),
+    };
+  }
+
+  @Patch(':id/cancel')
+  async cancel(@Param('id') id: string) {
+    await this.cancelNotification.execute({
+      notificationId: id,
+    });
+  }
+
+  @Patch(':id/read')
+  async read(@Param('id') id: string) {
+    await this.readNotification.execute({
+      notificationId: id,
+    });
+  }
+
+  @Patch(':id/unread')
+  async unread(@Param('id') id: string) {
+    await this.unreadNotification.execute({
+      notificationId: id,
+    });
   }
 }
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { NotificationsController } from './controllers/notifications.controller';
 import { SendNotification } from '@app/services/send-notification-service';
 import { DatabaseModule } from '../database/database.module';
+import { CancelNotification } from '@app/services/cancel-notification-service';
+import { CountRecipientNotifications } from '@app/services/count-recipient-notifications-service';
+import { GetRecipientNotifications } from '@app/services/get-recipient-notifications-service';
+import { ReadNotification } from '@app/services/read-notification-service';
+import { UnreadNotification } from '@app/services/unread-notification-service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HTTPModule {}
