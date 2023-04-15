93d72c33dc9ddf8b3c1df042afe9e806885e1e82
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-04-14 23:52:14 -0300

    Removed unutilized imports in count recipient notifications service

--- src/app/services/count-recipient-notifications.ts
+++ src/app/services/count-recipient-notifications.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { Content } from '../entities/notification/content';
-import { Notification } from '../entities/notification/notification';
 import { NotificationsRepository } from '../repositories/notification-repository';
 
 interface CountRecipientNotificationsRequest {
